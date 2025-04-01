b71689c201e3dc0c4a845e016847f6871b301e91
Swetasmita Chinaray <schinray@gmail.com>
2025-04-01 16:22:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+//import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+     
+      </div>
+    
+    </>
+  )
+}
+
+export default App
